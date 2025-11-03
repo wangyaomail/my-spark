cd5225119a775a1418a5cafb1e961568e72ce409
wangyaomail <wangyaomail@foxmail.com>
2025-11-03 19:05:08 +0800

    20251103

--- spark-2025/src/main/scala/C56T1.scala
+++ spark-2025/src/main/scala/C56T1.scala
@@ -85,3 +85,102 @@ object 求学生中出现的所有姓氏 extends StuBase{
       .foreach(println)
   }
 }
+
+object 求平均年龄 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    // 性能较差
+//    var d2 = data.map(2025-_(4).substring(0,4).toInt)
+//    println("平均年龄",d2.sum/d2.count)
+    // 性能好一些
+    val d = data.map(x=>(2025-x(4).substring(0,4).toInt, 1))
+      .reduce((x,y)=>(x._1+y._1, x._2+y._2))
+    println(d._1.toDouble/d._2)
+  }
+}
+
+object 成绩均值 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    val d = data.map(x=>(x(7).toInt, 1))
+      .filter(x=>x._1>=60 && x._1<80)
+      .reduce((x,y)=>(x._1+y._1, x._2+y._2))
+    println(d._1.toDouble/d._2)
+  }
+}
+
+object 求学生的年龄分布 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(2025-x(4).substring(0,4).toInt,1))
+      .groupBy(_._1)
+      .map(x=>(x._1,x._2.size))
+      .foreach(println)
+  }
+}
+
+object 求学生的月份分布 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(x(4).substring(5,7).toInt,1))
+      .groupBy(_._1)
+      .map(x=>(x._1,x._2.size))
+      .foreach(println)
+  }
+}
+
+object 电话大小排序 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(x(5).toLong,1))
+      .sortBy(_._1, false)
+      .foreach(println)
+  }
+}
+
+object 按学号后四位生日的数字和排序名字 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(x(2).takeRight(4).toInt+x(4).substring(5).replaceAll("-","").toInt,x(0)))
+      .sortBy(_._1, false)
+      .foreach(println)
+  }
+}
+
+object 索引生日姓名链表 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(x(4).substring(5),x(0)))
+      .groupBy(_._1)
+      .map(x=>(x._1,x._2.map(_._2)))
+      .sortBy(_._1)
+      .foreach(println)
+  }
+}
+
+
+object 索引河南各市的各区链表 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(x(6).take(6),x(6).substring(0,x(6).length-4)))
+      .filter(x=>x._1.startsWith("河南省"))
+      .filter(x=>x._1.length!=x._2.length)
+      .map(x=>(x._1.takeRight(3), x._2.takeRight(3)))
+      .groupBy(_._1)
+      .map(x=>(x._1,x._2.map(_._2).toList.distinct))
+      .sortBy(_._1)
+      .foreach(println)
+  }
+}
+
+object 求出生日最大的5个同学的名字 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(x(4).substring(5).replaceAll("-","").toInt,x(0)))
+      .sortBy(_._1,false)
+      .take(5)
+      .foreach(println)
+  }
+}
+
+object 求手机号按位加和最大的三个手机号 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(x(5).map(_.toInt-'0').sum,x(5)))
+      .sortBy(_._1,false)
+      .take(5)
+      .foreach(println)
+  }
+}
+
+
--- spark-2025/src/main/scala/C78T1.scala
+++ spark-2025/src/main/scala/C78T1.scala
@@ -93,3 +93,100 @@ object 输出手机号前三位的所有值 extends StuBase{
       .foreach(println)
   }
 }
+
+object 求平均年龄 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    // 性能较差
+//    var d2 = data.map(2025-_(4).substring(0,4).toInt)
+//    println("平均年龄",d2.sum()/d2.count())
+    // 性能较好
+    val d = data.map(x=>(2025-x(4).substring(0,4).toInt, 1))
+      .reduce((x,y)=>(x._1+y._1,x._2+y._2))
+    println(d, d._1.toDouble/d._2)
+
+  }
+}
+
+object 输出6080分数段的成绩均值 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    val d = data.map(x=>(x(7).toInt, 1))
+      .filter(x=>x._1>=60 & x._1<80)
+      .reduce((x,y)=>(x._1+y._1,x._2+y._2))
+    println(d, d._1.toDouble/d._2)
+  }
+}
+
+object 求学生的年龄分布 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(2025-x(4).substring(0,4).toInt, 1))
+      .groupBy(_._1)
+      .map(x=>(x._1,x._2.size))
+      .foreach(println)
+  }
+}
+
+object 求学生的月份分布 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(x(4).substring(5,7).toInt, 1))
+      .groupBy(_._1)
+      .map(x=>(x._1,x._2.size))
+      .foreach(println)
+  }
+}
+
+object 按电话号码排序 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(x(5).toLong, 1))
+      .sortBy(_._1)
+      .foreach(println)
+  }
+}
+
+object 复杂排序 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(x(2).substring(5).toInt, x(4).takeRight(5).replaceAll("-","") , x(0)))
+      .map(x=>(x._1+x._2.toInt, x._3))
+      .sortBy(_._1)
+      .foreach(println)
+  }
+}
+
+object 索引生日相同姓名 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(x(4).takeRight(5), x(0)))
+      .groupBy(_._1)
+      .map(x=>(x._1,x._2.map(_._2).mkString("、")))
+      .foreach(println)
+  }
+}
+
+object 索引河南省各市各区 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>x(6))
+      .filter(_.contains("河南省"))
+      .map(x=>(x.substring(3,6),x.substring(6,9) ))
+//      .filter(x=> ! (x._2.takeRight(0)>="0" & x._2.takeRight(0)<="9"))
+      .filter(x=>x._2.contains("县") || x._2.contains("区"))
+      .groupBy(_._1)
+      .map(x=>(x._1,x._2.map(_._2).toList.distinct))
+      .foreach(println)
+  }
+}
+object 求出生日最大的5个同学的名字 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(x(0),x(4).substring(5)))
+      .sortBy(_._2,false)
+      .take(5)
+      .foreach(println)
+  }
+}
+
+object 求手机号按位加和最大的三个手机号 extends StuBase{
+  def main(args: Array[String]): Unit = {
+    data.map(x=>(x(5),x(5).map(_.toInt-'0').sum))
+      .sortBy(_._2,false)
+      .take(3)
+      .foreach(println)
+  }
+}
+
